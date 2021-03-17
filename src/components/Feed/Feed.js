bacd1189b04891cfe63e1002bd6dfbcc51f5cfb3
Thagana <samuelmthwa79@gmail>
2021-03-17 20:54:10 +0200

    added components

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import styles from './Feed.style';
+
+export default function Feed(props) {
+    const { data } = props;
+    const Item = ({ image }) => {
+        return (
+            <TouchableOpacity>
+                <Image
+                    source={{ uri: image }}
+                    style={styles.image}
+                />
+            </TouchableOpacity>
+        )
+    }
+    const renderFeed = ({ item }) => (
+        <Item image={item.image} />
+    )
+    return (
+            <FlatList
+                numColumns={2}
+                columnWrapperStyle={styles.container}
+                data={data}
+                renderItem={renderFeed}
+                keyExtractor={( item ) => item.id}
+            />
+    )
+}
