f4e69b123cc035eac8b63f5a795cde18f46f4ea7
Thagana <samuelmthwa79@gmail>
2021-07-29 00:44:52 +0200

    lottie files

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import LottieView from 'lottie-react-native';
 
 import styles from './Loading.style';
 
 export default function Loading() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Loading ...</Text>
+            <LottieView autoPlay loop source={require('../../assets/loading.json')} />
         </View>
     )
 }
