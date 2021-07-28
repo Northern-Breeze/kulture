5d837b9b52b05fd37158517753bb20e08b2841bd
Thagana <samuelmthwa79@gmail>
2021-07-28 23:25:21 +0200

    adding smooth features

--- src/components/Feed/TopList/Item/Item.style.js
+++ src/components/Feed/TopList/Item/Item.style.js
@@ -6,7 +6,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#000',
     },
     backgroundImage: {
         width: wp(100),
