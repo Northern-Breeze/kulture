513908dd04e217918ba57b580a7c30047a9845e7
Thagana <samuelmthwa79@gmail>
2021-08-08 17:51:55 +0200

    live videos

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {createStackNavigator} from '@react-navigation/stack';
-import {useStoreState, useStoreActions} from 'easy-peasy';
+import {useStoreState} from 'easy-peasy';
 
 import HomeRoute from './HomeRoutes';
 import AuthRoute from './AuthRoutes';
@@ -10,27 +10,7 @@ import AuthRoute from './AuthRoutes';
 const RootStack = createStackNavigator();
 
 export default function Routes(props) {
-  const {navigation} = props;
-  const setIsLoggin = useStoreActions((actions) => actions.setIsLoggin);
   const isloggedIn = useStoreState((state) => state.isloggedIn);
-
-  // const checkToken = React.useCallback(async () => {
-  //   try {
-  //     const token = await AsyncStorage.getItem('token');
-  //     if (token !== null) {
-  //       setIsLoggin(true);
-  //     } else {
-  //       setIsLoggin(false);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, [isloggedIn]);
-
-  // React.useEffect(() => {
-  //   checkToken();
-  // }, [checkToken]);
-
   return (
     <RootStack.Navigator
       screenOptions={{
