9a658d86c9d4ce06303d4aba81c0a3bf3336ebc4
Thagana <samuelmthwa79@gmail>
2021-08-08 21:09:17 +0200

    author of post

--- src/screens/Tab/Live/Live.js
+++ src/screens/Tab/Live/Live.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import Video from 'react-native-video';
+
+import styles from './Tab.style';
 
 export default function Live() {
-    return (
-        <View>
-            <Text>Live</Text>
-        </View>
-    )
+  const [paused, setPaused] = React.useState(true);
+
+  // refs
+  const videoRef = React.useRef();
+
+  // Events
+  const onBuffer = (buffer) => {
+    console.log('buffer: ', buffer);
+  };
+
+  const handleError = (error) => {
+    console.log('error', error);
+  };
+
+  const handleVideoPress = () => {
+    setPaused(false);
+  };
+  
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={handleVideoPress} style={styles.videoContainer}>
+        <Video
+          poster="https://northernbreeze-bucket.sfo3.digitaloceanspaces.com/android-chrome-512x512.png"
+          source={{
+            uri:
+              'https://northernbreeze-bucket.sfo3.digitaloceanspaces.com/kulture-videos/Nature%20Beautiful%20short%20video%20720p%20HD.mp4',
+          }}
+          ref={videoRef}
+          paused={paused}
+          onBuffer={onBuffer}
+          onError={handleError}
+          style={styles.backgroundVideo}
+          repeat
+        />
+      </Pressable>
+    </View>
+  );
 }
