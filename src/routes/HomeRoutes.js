5e070d93c1e0787eb027604170def8414f9bd8f3
Thagana <samuelmthwa79@gmail>
2021-08-08 19:10:46 +0200

    home routes

--- src/routes/HomeRoutes.js
+++ src/routes/HomeRoutes.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Preview from '../screens/Preview';
 
 const Stack = createStackNavigator();
 
 export default function HomeRoute() {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="preview" component={Preview} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen name="preview" component={Preview} />
+    </Stack.Navigator>
+  );
 }
