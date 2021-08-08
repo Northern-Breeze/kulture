acf5bfabd5fabd535d10d02bbbeee5864c620e67
Thagana <samuelmthwa79@gmail>
2021-08-09 01:52:50 +0200

    video view

--- src/components/Video/Video.style.js
+++ src/components/Video/Video.style.js
@@ -3,14 +3,12 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+
 const styles = StyleSheet.create({
-  videoContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  
+  backgroundVideo: {
     marginVertical: hp(0.5),
     marginHorizontal: wp(2),
-  },
-  backgroundVideo: {
     width: wp(95),
     height: hp(50),
     borderRadius: 5,
