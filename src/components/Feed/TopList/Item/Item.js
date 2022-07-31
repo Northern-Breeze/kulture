9ef411338b7e07f4f0478406405ca78560d2ef93
Thagana <samuelmthwa79@gmail>
2022-07-31 14:15:08 +0200

    home view update

--- src/components/Feed/TopList/Item/Item.js
+++ src/components/Feed/TopList/Item/Item.js
@@ -1,49 +1,24 @@
 import * as React from 'react';
-import {
-  TouchableOpacity,
-  Image,
-  ImageBackground,
-  View,
-  Text,
-  Pressable,
-} from 'react-native';
+import {TouchableOpacity, Image} from 'react-native';
 import styles from './Item.style';
-
-import server from '../../../../service/server';
 
 export default function Item(props) {
   // Props
-  const {item, index, activeIndex, navigation} = props;
+  const {item, navigation} = props;
+
+  const randomBool = React.useMemo(() => Math.random() < 0.5, []);
 
   // Refs
   const mounted = React.useRef(true);
-  const liked = React.useRef(false);
 
-  const handleProfilePress = () => {
-    navigation.navigate('Profile');
-  }
   const handlePostPress = () => {
-    navigation.navigate('Preview', { username: item.username, avatar: item.avatar, image: item.image, userId: item.userId });
-  }
-  React.useEffect(() => {
-    if (index === activeIndex) {
-      if (typeof item.postId !== 'undefined') {
-        setTimeout(() => {
-          if (!liked.current) {
-            liked.current = true;
-            server
-              .likeAPost({
-                postId: item.postId,
-              })
-              .then()
-              .catch((error) => {
-                console.log(error);
-              });
-          }
-        }, 8000);
-      }
-    }
-  }, [activeIndex]);
+    navigation.navigate('Preview', {
+      username: item.username,
+      avatar: item.avatar,
+      image: item.image,
+      userId: item.userId,
+    });
+  };
 
   React.useEffect(() => {
     return () => {
@@ -52,27 +27,15 @@ export default function Item(props) {
   }, []);
 
   return (
-    <TouchableOpacity activeOpacity={0.9} style={styles.container} onPress={handlePostPress}>
-      <ImageBackground
-        source={{uri: item.image}}
-        style={styles.backgroundImage}
-        blurRadius={80}>
-        <View style={styles.nameContainer}>
-          <Pressable style={styles.usernameContainer} onPress={handleProfilePress}>
-            <Image source={{uri: item.avatar}} style={styles.avatar} />
-          </Pressable>
-          <View
-            style={styles.tagContainer}>
-            <Pressable
-              style={styles.tag}>
-              <Text style={styles.tagText}>
-                @{item.username}
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-        <Image source={{uri: item.image}} style={styles.fullImage} />
-      </ImageBackground>
+    <TouchableOpacity
+      activeOpacity={0.9}
+      style={styles.container}
+      onPress={handlePostPress}>
+      <Image
+        source={{uri: item.avatar}}
+        style={[styles.fullImage, {height: randomBool ? 150 : 280}]}
+        resizeMode="cover"
+      />
     </TouchableOpacity>
   );
 }
--- src/components/Feed/TopList/TopList.js
+++ src/components/Feed/TopList/TopList.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import {FlatList, Dimensions} from 'react-native';
-import Footer from '../Footer';
+import MasonryList from '@react-native-seoul/masonry-list';
 
 import Item from './Item';
 
-const {width} = Dimensions.get('screen');
-
 export default function TopList(props) {
   // Props
-  const {posts, topRef, scrollToActiveIndex, activeIndex, handleLoadMore, navigation} = props;
+  const {
+    users,
+    activeIndex,
+    handleLoadMore,
+    navigation,
+  } = props;
   return (
-    <FlatList
-      ref={topRef}
-      data={posts}
+    <MasonryList
+      data={users}
       keyExtractor={(_, index) => index.toString() + Math.random().toString()}
-      horizontal
-      pagingEnabled
-      ListFooterComponent={<Footer />}
-      onEndReached={handleLoadMore}
-      showsHorizontalScrollIndicator={false}
-      onEndReachedThreshold={0.05}
-      initialNumToRender={6}
-      onMomentumScrollEnd={(ev) => {
-        scrollToActiveIndex(
-          Math.floor(
-            Math.floor(ev.nativeEvent.contentOffset.x) / Math.floor(width),
-          ),
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      renderItem={({item, index}) => {
+        return (
+          <Item
+            item={item}
+            index={index}
+            activeIndex={activeIndex}
+            navigation={navigation}
+          />
         );
       }}
-      renderItem={({item, index}) => {
-        return <Item item={item} index={index} activeIndex={activeIndex} navigation={navigation}/>;
-      }}
+      onEndReachedThreshold={0.05}
+      onEndReached={handleLoadMore}
     />
   );
 }
